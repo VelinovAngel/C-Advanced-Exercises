a6e08899d6e33e6cd65f4309761d5879e2922096
Angel <kakarum1992@gmail.com>
2020-09-19 14:18:35 +0200

    new exercise

--- 07.Truck_Tour/Program.cs
+++ 07.Truck_Tour/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace _07.Truck_Tour
 {
@@ -6,7 +8,52 @@ namespace _07.Truck_Tour
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int n = int.Parse(Console.ReadLine());
+
+            Queue<int[]> pumps = new Queue<int[]>();
+
+            int count = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                int[] currPump = Console.ReadLine()
+                    .Split()
+                    .Select(int.Parse)
+                    .ToArray();
+
+                pumps.Enqueue(currPump);
+            }
+
+            while (true)
+            {
+                bool foundPoint = true;
+                int fuelAmount = 0;
+
+                for (int i = 0; i < n; i++)
+                {
+                    int[] currPupm = pumps.Dequeue();
+                    fuelAmount += currPupm[0];
+
+                    if (fuelAmount < currPupm[1])
+                    {
+                        foundPoint = false;
+                    }
+
+                    fuelAmount -= currPupm[1];
+                    pumps.Enqueue(currPupm);
+                }
+
+                if (foundPoint)
+                {
+                    break;
+                }
+                count++;
+
+                pumps.Enqueue(pumps.Dequeue());
+
+            }
+
+            Console.WriteLine(count);
         }
     }
 }
