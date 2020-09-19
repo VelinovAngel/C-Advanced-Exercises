0826a72e959896120c8c2602da3cf17d2067da93
Angel <kakarum1992@gmail.com>
2020-09-19 18:00:12 +0200

    New Exercise

---
+++ 09.Simple_Text_Editor/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _09.Simple_Text_Editor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int commandCount = int.Parse(Console.ReadLine());
+
+            Stack<string> version = new Stack<string>();
+            StringBuilder text = new StringBuilder();
+
+            for (int i = 0; i < commandCount; i++)
+            {
+                string[] commandProps = Console.ReadLine().Split();
+
+                string command = commandProps[0];
+
+                switch (command)
+                {
+                    case "1":
+                        version.Push(text.ToString());
+                        string textToAdd = commandProps[1];
+                        text.Append(textToAdd);
+                        break;
+
+                    case "2":
+                        version.Push(text.ToString());
+                        int removeElements = int.Parse(commandProps[1]);
+                        text.Remove(text.Length - removeElements , removeElements);
+                        break;
+
+                    case "3":
+                        int index = int.Parse(commandProps[1]) - 1;
+                        Console.WriteLine(text[index]);
+                        break;
+
+                    case "4":
+                        text.Clear();
+                        text.Append(version.Pop());
+                        break;
+                }
+
+            }
+
+        }
+    }
+}
