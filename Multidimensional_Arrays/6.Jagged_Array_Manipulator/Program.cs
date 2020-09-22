7bfec924db8bd32183b971c0e619f564c9f884da
Angel <kakarum1992@gmail.com>
2020-09-22 23:18:05 +0200

    new exercise

--- Multidimensional_Arrays/6.Jagged_Array_Manipulator/Program.cs
+++ Multidimensional_Arrays/6.Jagged_Array_Manipulator/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace _6.Jagged_Array_Manipulator
 {
@@ -6,7 +7,33 @@ namespace _6.Jagged_Array_Manipulator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
+        }
+
+        static void FillMatrix(string[,] matrix)
+        {
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                string[] currentRows = Console.ReadLine()
+                    .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                    .ToArray();
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = currentRows[col];
+                }
+            }
+        }
+
+        static void PrintMatrix(char[,] matrix)
+        {
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    Console.Write(matrix[row, col]);
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
---
+++ Stacks and queus/5.Print_Even_Numbers/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _5.Print_Even_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            Queue<int> queue = new Queue<int>(numbers);
+            string output = string.Empty;
+            while (queue.Count > 0)
+            {
+                int item = queue.Peek();
+                if (item % 2 == 0)
+                {
+                    output += queue.Dequeue() + ", ";
+
+                }
+                else
+                {
+                    queue.Dequeue();
+                }
+            }
+
+            Console.WriteLine(output.Substring(0, output.Length - 2));
+        }
+    }
+}
