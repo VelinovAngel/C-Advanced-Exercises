22e2e79947bf630c231f0ca1fabe45c3d43dc64f
Angel <kakarum1992@gmail.com>
2020-09-23 22:44:41 +0200

    new exercise

--- Stacks and queus/4.Matching_Brackets/Program.cs
+++ Stacks and queus/4.Matching_Brackets/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace _4.Matching_Brackets
 {
@@ -6,7 +7,22 @@ namespace _4.Matching_Brackets
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-        }
+            string expression = Console.ReadLine();
+
+            Stack<int> stack = new Stack<int>();
+
+            for (int i = 0; i < expression.Length; i++)
+            {
+                if (expression[i] == '(')
+                {
+                    stack.Push(i);
+                }
+
+                if (expression[i] == ')')
+                {
+                    int start = stack.Pop();
+                    Console.WriteLine(expression.Substring(start, i - start + 1));
+                }
+            }
     }
 }
