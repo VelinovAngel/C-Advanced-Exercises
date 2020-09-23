ee53bd18494a5b6cefd356fcde103a0f16c629e9
Angel <kakarum1992@gmail.com>
2020-09-23 22:46:22 +0200

    new exercise

---
+++ Stacks and queus/8.Traffic_Jam/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _8.Traffic_Jam
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
