46202999af81488fa898c48d54d857336fc70943
Angel <kakarum1992@gmail.com>
2020-09-26 17:34:37 +0200

    Product Shop

---
+++ Sets_and_Dictionaries_Advanced/03.Product_Shop/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03.Product_Shop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = string.Empty;
+
+            Dictionary<string, Dictionary<string, double>> shops = new Dictionary<string, Dictionary<string, double>>();
+
+            while ((input = Console.ReadLine()) != "Revision")
+            {
+                string[] tokens = input
+                    .Split(", ", StringSplitOptions.RemoveEmptyEntries);
+
+                string shop = tokens[0];
+                string product = tokens[1];
+                double price = double.Parse(tokens[2]);
+
+                if (!shops.ContainsKey(shop))
+                {
+                    shops.Add(shop, new Dictionary<string, double>());
+                    if (!shops[shop].ContainsKey(product))
+                    {
+                        shops[shop].Add(product, price);
+                    }
+                }
+                else
+                {
+                    shops[shop].Add(product, price);
+                }
+            }
+
+            foreach (var kvp in shops.OrderBy(x => x.Key))
+            {
+                Console.WriteLine($"{kvp.Key}->");
+
+                foreach (var item in kvp.Value)
+                {
+                    Console.WriteLine($"Product: {item.Key}, Price: {item.Value}");
+                }
+            }
+        }
+    }
+}
