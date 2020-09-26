573407a659686764cbc116c7660c121c7b322ee8
Angel <kakarum1992@gmail.com>
2020-09-26 12:40:53 +0200

    Average Student Grades

---
+++ Sets_and_Dictionaries_Advanced/02.Average_Student_Grades/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _02.Average_Student_Grades
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numOfStudents = int.Parse(Console.ReadLine());
+            Dictionary<string, List<decimal>> grades = new Dictionary<string, List<decimal>>();
+
+
+            for (int i = 0; i < numOfStudents; i++)
+            {
+                string[] input = Console.ReadLine()
+                    .Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+                string student = input[0];
+                decimal grade = decimal.Parse(input[1]);
+
+                if (!grades.ContainsKey(student))
+                {
+                    grades.Add(student, new List<decimal>() { grade });
+                }
+                else
+                {
+                    grades[student].Add(grade);
+                }
+            }
+
+            foreach (var kvp in grades)
+            {
+                StringBuilder allGrades = new StringBuilder();
+                for (int i = 0; i < kvp.Value.Count; i++)
+                {
+                    allGrades.Append($"{kvp.Value[i]:f2} ");
+                }
+                Console.WriteLine($"{kvp.Key} -> {allGrades}(avg: {kvp.Value.Average():f2})");
+            }
+        }
+    }
+}
