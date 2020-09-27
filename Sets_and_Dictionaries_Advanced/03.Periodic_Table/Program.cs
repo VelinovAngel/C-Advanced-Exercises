16f7352da4a000aeede9e3f64e5084f9112228bf
Angel <kakarum1992@gmail.com>
2020-09-27 14:57:24 +0200

    Periodic Table Exercise

---
+++ Sets_and_Dictionaries_Advanced/03.Periodic_Table/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03.Periodic_Table
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            SortedSet<string> table = new SortedSet<string>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] input = Console.ReadLine()
+                    .Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+                for (int j = 0; j < input.Length; j++)
+                {
+                    table.Add(input[j]);
+                }
+            }
+
+            foreach (var item in table)
+            {
+                Console.Write($"{item} ");
+            }
+
+        }
+    }
+}
