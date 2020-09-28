a94b6712c2a4b32544a7cc79b08e004f4b78c623
Angel <kakarum1992@gmail.com>
2020-09-28 22:56:50 +0200

    Exam Prep

---
+++ ExamPreparationAdvanced/02.Bee/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+
+namespace _02.Bee
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            char[,] beeTerritory = new char[n, n];
+
+            int beeRow = -1;
+            int beeCol = -1;
+
+            for (int row = 0; row < n; row++)
+            {
+                char[] currentRow = Console.ReadLine().ToCharArray();
+
+                for (int col = 0; col < n; col++)
+                {
+                    beeTerritory[row, col] = currentRow[col];
+                    if (currentRow[col] == 'B')
+                    {
+                        beeRow = row;
+                        beeCol = col;
+                    }
+                }
+            }
+
+            int pollinatedFlowers = 0;
+            string input = Console.ReadLine();
+
+            while (input != "End")
+            {
+                //"up", "down", "left", "right",
+                beeTerritory[beeRow, beeCol] = '.';
+
+                if (input == "up")
+                {
+                    beeRow--;
+                }
+                else if (input == "down")
+                {
+                    beeRow++;
+                }
+                else if (input == "left")
+                {
+                    beeCol--;
+                }
+                else if (input == "right")
+                {
+                    beeCol++;
+                }
+
+                if (beeCol > n - 1 || beeCol < 0 || beeRow < 0 || beeRow > n - 1)
+                {
+                    Console.WriteLine("The bee got lost!");
+                    break;
+                }
+
+                if (beeTerritory[beeRow, beeCol] == 'f')
+                {
+                    pollinatedFlowers++;
+                }
+                else if (beeTerritory[beeRow, beeCol] == 'O')
+                {
+                    continue;
+                }
+
+                input = Console.ReadLine();
+            }
+            if (input == "End")
+            {
+                beeTerritory[beeRow, beeCol] = '.';
+                beeTerritory[beeRow, beeCol] = 'B';
+            }
+
+            if (pollinatedFlowers < 5)
+            {
+                Console.WriteLine($"The bee couldn't pollinate the flowers, she needed {5 - pollinatedFlowers} flowers more");
+            }
+            else
+            {
+                Console.WriteLine($"Great job, the bee managed to pollinate {pollinatedFlowers} flowers!");
+            }
+
+            for (int row = 0; row < n; row++)
+            {
+                for (int col = 0; col < n; col++)
+                {
+                    Console.Write(beeTerritory[row, col]);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ Sets_and_Dictionaries_Advanced/07.The_V-Logger/Program.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _07.The_V_Logger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = string.Empty;
+            HashSet<Vlogger> vloggers = new HashSet<Vlogger>();
+
+            while ((input = Console.ReadLine()) != "Statistics")
+            {
+
+                string[] tokens = input
+                    .Split();
+
+                string action = tokens[1];
+                string name = tokens[0];
+
+                switch (action)
+                {
+                    case "joined":
+                        Vlogger newVlogger = new Vlogger(name);
+                        if (!vloggers.Any(x => x.Name == newVlogger.Name))
+                        {
+                            vloggers.Add(newVlogger);
+                        }
+                        break;
+                    case "followed":
+                        string following = tokens[0];
+                        string followed = tokens[2];
+
+                        if (following != followed
+                            && vloggers.Any(x => x.Name == following)
+                            && vloggers.Any(x => x.Name == followed))
+                        {
+                            Vlogger vlogger1 = vloggers.Where(v => v.Name == following).FirstOrDefault();
+                            vlogger1.AddFollowing(followed);
+
+                            Vlogger vlogger2 = vloggers.Where(v => v.Name == followed).FirstOrDefault();
+                            vlogger2.AddFollower(following);
+                        }
+                        break;
+                }
+            }
+
+            Console.WriteLine($"The V-Logger has a total of {vloggers.Count} vloggers in its logs.");
+
+            if (vloggers.Count == 0)
+            {
+                return;
+            }
+
+            int maxFollowers = int.MinValue;
+
+            foreach (var item in vloggers)
+            {
+                if (item.Followers.Count > maxFollowers)
+                {
+                    maxFollowers = item.Followers.Count;
+                }       
+            }
+
+            Vlogger famous = vloggers.Where(v => v.Followers.Count == maxFollowers).FirstOrDefault();
+ 
+            Console.WriteLine($"1. {famous.Name} : {famous.Followers.Count} followers, {famous.Following.Count} following");
+            foreach (var follower in famous.Followers)
+            {
+                Console.WriteLine($"*  {follower}");
+            }
+
+            vloggers.Remove(famous);
+
+            int cout = 2;
+            foreach (var item in vloggers.OrderByDescending(x=>x.Followers.Count).ThenBy(x=>x.Following.Count))
+            {
+                Console.WriteLine($"{cout}. {item.Name} : {item.Followers.Count} followers, {item.Following.Count} following");
+                cout++;
+            }
+        }
+
+
+    }
+    public class Vlogger
+    {
+        public string Name { get; set; }
+
+        public SortedSet<string> Followers { get; set; }
+
+        public HashSet<string> Following { get; set; }
+
+        public Vlogger(string name)
+        {
+            this.Name = name;
+            this.Followers = new SortedSet<string>();
+            this.Following = new HashSet<string>();
+        }
+
+        public void AddFollower(string name)
+        {
+            Followers.Add(name);
+        }
+
+        public void AddFollowing(string name)
+        {
+            Following.Add(name);
+        }
+    }
+}
