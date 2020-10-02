0361b960bcac8bcdd01449ada532d1b6f3abc78f
Angel <kakarum1992@gmail.com>
2020-10-02 23:59:53 +0200

    Read from file with File Class and created new file modified

---
+++ STREAMS_FILES_AND_DIRECTORIES/0.2.Line_Numbers/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+
+namespace _0._2.Line_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] text = File.ReadAllLines("../../../text.txt");
+            string[] newText = new string[text.Length];
+
+            for (int i = 0; i < text.Length - 1; i++)
+            {
+                string line = text[i];
+                int countSymbols = 0;
+                int countPunctuation = 0;
+                for (int j = 0; j < line.Length; j++)
+                {
+                    char symbol = line[j];
+                    if (char.IsLetter(symbol))
+                    {
+                        countSymbols++;
+                    }
+                    if (char.IsPunctuation(symbol))
+                    {
+                        countPunctuation++;
+                    }
+                }
+                Console.WriteLine($"Line {i + 1}: {text[i]} ({countSymbols})({countPunctuation})");
+                newText[i] = $"Line {i + 1}: {text[i]} ({countSymbols})({countPunctuation})";
+                File.WriteAllLines("../../../OutPutText.txt", newText);
+            }
+        }
+    }
+}
