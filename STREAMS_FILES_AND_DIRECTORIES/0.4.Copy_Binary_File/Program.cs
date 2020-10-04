0a45e286d699883c89437cf3ea7040f7b9e47697
Angel <kakarum1992@gmail.com>
2020-10-04 11:25:28 +0200

    Copy file through binary method

---
+++ STREAMS_FILES_AND_DIRECTORIES/0.4.Copy_Binary_File/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+
+namespace _0._4.Copy_Binary_File
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            using (FileStream stream = File.OpenRead("../../../input.jpg"))
+            using (FileStream writeStream = File.OpenWrite("../../../output.jpg"))
+            {
+                BinaryReader reader = new BinaryReader(stream);
+                BinaryWriter writer = new BinaryWriter(writeStream);
+
+                byte[] buffer = new Byte[1024];
+                int bytesRead;
+
+                while ((bytesRead = stream.Read(buffer, 0, 1024)) > 0)
+                {
+                    writeStream.Write(buffer, 0, bytesRead);
+                }
+            }
+        }
+    }
+}
