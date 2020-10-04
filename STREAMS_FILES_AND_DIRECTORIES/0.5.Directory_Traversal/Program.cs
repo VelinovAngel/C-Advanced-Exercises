bb521a809310a3fd23d92ba49432a9f51482f529
Angel <kakarum1992@gmail.com>
2020-10-04 11:27:03 +0200

    Info for any files of a directory

---
+++ STREAMS_FILES_AND_DIRECTORIES/0.5.Directory_Traversal/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace _0._5.Directory_Traversal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, Dictionary<string, double>> fileInfo = new Dictionary<string, Dictionary<string, double>>();
+
+            DirectoryInfo directoryFile = new DirectoryInfo("../../../");
+            FileInfo[] files = directoryFile.GetFiles();
+
+            foreach (var file in files)
+            {
+                if (!fileInfo.ContainsKey(file.Extension))
+                {
+                    fileInfo.Add(file.Extension, new Dictionary<string, double>());
+                }
+
+                fileInfo[file.Extension].Add(file.Name, file.Length / 1000.00);
+            }
+
+            
+
+            using (StreamWriter writer =
+                new StreamWriter
+                (@$"{Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory)}DirectoryTraversal.txt"))
+            {
+
+                foreach (var kvp in fileInfo.OrderByDescending(x => x.Value.Count).ThenBy(n => n.Key))
+                {
+                    writer.WriteLine($"{kvp.Key}");
+                    foreach (var item in kvp.Value.OrderBy(x => x.Value))
+                    {
+                        writer.WriteLine($"--{item.Key} - {item.Value}kb");
+                    }
+                }
+            }
+
+        }
+    }
+}
