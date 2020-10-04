5edcc7b532c0a10281898549aa0aaf9172c8a641
Angel <kakarum1992@gmail.com>
2020-10-04 19:30:00 +0200

    Exercises on FP

---
+++ Functional_Programming/04.Find_Evens_or_Odds/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _04.Find_Evens_or_Odds
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            string command = Console.ReadLine();
+
+            int start = input[0];
+            int end = input[1];
+
+            Func<int, int, List<int>> generateList = (start, end) =>
+              {
+                  List<int> nums = new List<int>();
+                  for (int i = start; i <= end; i++)
+                  {
+                      nums.Add(i);
+                  }
+                  return nums;
+              };
+
+            List<int> numbers = generateList(start, end);
+
+            Predicate<int> predicate = n => n % 2 == 0;
+            //Func<int, bool> evenPredicate = n => n % 2 == 0;
+
+            //numbers = numbers.Where(evenPredicate).ToList();
+
+            if (command == "odd")
+            {
+                predicate = n => n % 2 != 0;
+            }
+
+            numbers = MyWhere(numbers, predicate);
+            
+            Console.WriteLine(string.Join(' ', numbers));
+        }
+
+        static List<int> MyWhere(List<int> items, Predicate<int> predicate)
+        {
+            List<int> result = new List<int>();
+
+            foreach (var item in items)
+            {
+                if (predicate(item))
+                {
+                    result.Add(item);
+                }
+            }
+            return result;
+        }
+    }
+}
---
+++ Functional_Programming/05.Applied_Arithmetics/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _05.Applied_Arithmetics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<int, int> addFunc = x => x + 1;
+            Func<int, int> multiplyFunc = x => x * 2;
+            Func<int, int> subtractFunc = x => x - 1;
+
+
+            Action<List<int>> printFunc = x => Console.WriteLine(string.Join(' ', x));
+
+            //•	"add"->add 1 to each number
+            //•	"multiply"->multiply each number by 2
+            //•	"subtract"->subtract 1 from each number
+            //•	"print"->print the collection
+            //•	"end"->ends the input
+
+            List<int> numbers = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList();
+
+            string input = string.Empty;
+
+            while ((input = Console.ReadLine()) != "end")
+            {
+
+                switch (input)
+                {
+                    case "add":
+                        //numbers = numbers.Select(x => addFunc(x)).ToList();
+                        numbers = numbers.Select(addFunc).ToList();
+                        break;
+                    case "multiply":
+                        numbers = numbers.Select(multiplyFunc).ToList();
+                        break;
+                    case "subtract":
+                        numbers = numbers.Select(subtractFunc).ToList();
+                        break;
+                    case "print":
+                        printFunc(numbers);
+                        break;
+                }
+            }
+
+        }
+    }
+}
