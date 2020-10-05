970b182f1a580ba981929e07f265ddcd6afb7f4c
Angel <kakarum1992@gmail.com>
2020-10-05 18:04:27 +0200

    Exercises on FP

---
+++ Functional_Programming/09.List_Of_Predicates/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _09.List_Of_Predicates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            List<int> divisors = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .Distinct()
+                .ToList();
+            List<int> divisibleNumbers = GetDivisibleNumbers(n, divisors);
+
+            Console.WriteLine(string.Join(" ", divisibleNumbers));
+        }
+
+        static List<int> GetDivisibleNumbers(int n, List<int> divisors)
+        {
+            List<int> divisibleNumbers = new List<int>();
+
+            for (int num = 1; num <= n; num++)
+            {
+                bool isDivisible = true;
+
+                foreach (var d in divisors)
+                {
+                    Predicate<int> isNotDivisor = x => num % x != 0;
+
+                    if (isNotDivisor(d))
+                    {
+                        isDivisible = false;
+                        break;
+                    }
+                }
+
+                if (isDivisible)
+                {
+                    divisibleNumbers.Add(num);
+                }
+            }
+            return divisibleNumbers;
+
+        }
+    }
+}
---
+++ Functional_Programming/11.The_Party_Reservation_Filter_Module/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _11.The_Party_Reservation_Filter_Module
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> people = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .ToList();
+
+
+            string input = string.Empty;
+            List<string> filters = new List<string>();
+
+            while ((input = Console.ReadLine()) != "Print")
+            {
+                if (input.StartsWith("Add filter;"))
+                {
+                    filters.Add(input);
+                }
+                else if (input.StartsWith("Remove filter;"))
+                {
+                    filters.Remove(input);
+                }
+            }
+
+            foreach (var filterLine in filters)
+            {
+                string[] tokens = filterLine.Split(';');
+                string commnadType = tokens[0];
+
+
+            }
+
+        }
+    }
+}
