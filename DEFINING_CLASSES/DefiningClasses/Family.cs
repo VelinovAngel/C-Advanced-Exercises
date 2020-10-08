5ae63d6d14a78c2c1be1e518cdc730058f4b775a
Angel <kakarum1992@gmail.com>
2020-10-08 18:54:45 +0200

    exercis with Classes

---
+++ DEFINING_CLASSES/DefiningClasses/Family.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DefiningClasses
+{
+    public class Family
+    {
+        public Family()
+        {
+            this.People = new List<Person>();
+        }
+        public List<Person> People { get; set; }
+
+
+        public void AddMember(Person member)
+        {
+            People.Add(member);
+        }
+
+        public Person GetOldestMember()
+        {
+            int maxAge = int.MinValue;
+            Person person = null;
+
+            foreach (var item in People)
+            {
+                int currAge = item.Age;
+
+                if (currAge > maxAge)
+                {
+                    maxAge = currAge;
+                    person = item;
+                }
+            }
+
+            return person;
+            
+        }
+    }
+}
