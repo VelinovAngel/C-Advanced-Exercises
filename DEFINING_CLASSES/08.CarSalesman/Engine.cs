0d8009c2b6fe442d3b91b3ecec77c19edc2eca6b
Angel <kakarum1992@gmail.com>
2020-10-10 23:33:54 +0200

    add class Engine

---
+++ DEFINING_CLASSES/08.CarSalesman/Engine.cs
@@ -0,0 +1,36 @@
+﻿using System;
+namespace _08.CarSalesman
+{
+    public class Engine
+    {
+        public Engine(string model, int power)
+        {
+            this.Model = model;
+            this.Power = power;
+        }
+        public Engine(string model, int power, int efficiency)
+            : this(model, power)
+        {
+
+            this.Efficiency = efficiency;
+        }
+        public Engine(string model, int power, int displacement, int efficiency)
+            : this(model, power, displacement)
+        {
+            this.Displacement = displacement;
+        }
+
+        //•	Model
+        //•	Power
+        //•	Displacement
+        //•	Efficiency
+
+        public string Model { get; set; }
+        public int Power { get; set; }
+
+        public int Displacement { get; set; }
+        public int Efficiency { get; set; }
+
+
+    }
+}
