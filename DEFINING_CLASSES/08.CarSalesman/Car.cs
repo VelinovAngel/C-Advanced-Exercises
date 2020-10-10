e9fe8bcabcbe3cfc61a1086c7d24daf76d0e5c88
Angel <kakarum1992@gmail.com>
2020-10-10 23:35:12 +0200

    add class Car

---
+++ DEFINING_CLASSES/08.CarSalesman/Car.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace _08.CarSalesman
+{
+    public class Car
+    {
+        public Car(string model, string engine, int weight, string color)
+        {
+            Model = model;
+            Engine = engine;
+            Weight = weight;
+            Color = color;
+        }
+
+        //•	Model
+        //•	Engine
+        //•	Weight 
+        //•	Color
+
+        public string Model { get; set; }
+        public string Engine { get; set; }
+        public int Weight { get; set; }
+        public string Color { get; set; }
+    }
+}
