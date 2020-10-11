9d5ac9808efdb5575bf9e6be315e50cb8703f755
Angel <kakarum1992@gmail.com>
2020-10-11 11:57:16 +0200

    add override ToString on Car class

--- DEFINING_CLASSES/08.CarSalesman/Car.cs
+++ DEFINING_CLASSES/08.CarSalesman/Car.cs
@@ -1,14 +1,32 @@
 ﻿using System;
+using System.Text;
+
 namespace _08.CarSalesman
 {
     public class Car
     {
-        public Car(string model, string engine, int weight, string color)
+        public Car(string model,Engine engine)
         {
-            Model = model;
-            Engine = engine;
-            Weight = weight;
-            Color = color;
+            this.Model = model;
+            this.Engine = engine;
+        }
+
+        public Car(string model, Engine engine, double weight)
+            :this(model,engine)
+        {
+            this.Weight = weight;
+        }
+
+        public Car(string model, Engine engine, string color)
+            :this(model,engine)
+        {
+            this.Color = color;
+        }
+        public Car(string model, Engine engine, double weight,string color)
+            :this(model,engine)
+        {
+            this.Weight = weight;
+            this.Color = color;
         }
 
         //•	Model
@@ -17,8 +35,33 @@ namespace _08.CarSalesman
         //•	Color
 
         public string Model { get; set; }
-        public string Engine { get; set; }
-        public int Weight { get; set; }
+        public Engine Engine { get; set; }
+        public double? Weight { get; set; }
         public string Color { get; set; }
+
+        //{CarModel}:
+        //  {EngineModel}:
+        //Power: { EnginePower}
+        //Displacement: { EngineDisplacement}
+        //Efficiency: { EngineEfficiency}
+        //Weight: { CarWeight}
+        //Color: { CarColor}
+
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            string weightStr = this.Weight.HasValue ? this.Weight.ToString() : "n/a";
+            string colorStr = String.IsNullOrEmpty(this.Color) ? "n/a" : this.Color;
+
+            sb.AppendLine($"{this.Model}:")
+                .AppendLine($"  {this.Engine}:")
+                .AppendLine($"  Weight: {weightStr}")
+                .AppendLine($"  Color: {colorStr}");
+
+            return sb.ToString().TrimEnd();
+
+        }
     }
+
 }
