b130aa583d89b7d6d1cfb48626cd87a1667f5507
Angel <kakarum1992@gmail.com>
2020-10-13 22:32:07 +0200

    Exam exercise

---
+++ ExamPreparationAdvanced/02.SpaceStation/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _02.SpaceStation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] matrix = new int[4, 4];
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    Console.Write(matrix[row,col]);
+                }
+                Console.WriteLine();
+            }
+
+        }
+    }
+}
