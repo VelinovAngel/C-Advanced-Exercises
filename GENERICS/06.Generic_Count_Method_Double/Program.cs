0c0087e7b0c308827c688d15c34e3b0be83af590
Angel <kakarum1992@gmail.com>
2020-10-15 21:35:50 +0200

    Generics exercises

---
+++ GENERICS/06.Generic_Count_Method_Double/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _06.Generic_Count_Method_Double
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<double> doubleColl = new List<double>();
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                double currNum = double.Parse(Console.ReadLine());
+                doubleColl.Add(currNum);
+            }
+
+            double compereNum = double.Parse(Console.ReadLine());
+            Box<double> box = new Box<double>(doubleColl);
+
+            Console.WriteLine(box.GetCountOfGreaterValues(compereNum));
+        }
+    }
+}
