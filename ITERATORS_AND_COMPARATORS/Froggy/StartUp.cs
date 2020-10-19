843d3b468779cfe569e5dbeb93bc860c2a9c3cf1
Angel <kakarum1992@gmail.com>
2020-10-19 18:12:02 +0200

    Iterators and comparators exercises

---
+++ ITERATORS_AND_COMPARATORS/Froggy/StartUp.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Froggy
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            int[] stones = Console.ReadLine()?
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            Lake lake = new Lake(stones);
+
+            //Console.WriteLine(String.Join(", ", lake));
+
+            List<int> path = new List<int>();
+            foreach (var stone in lake)
+            {
+                path.Add(stone);
+            }
+
+            Console.WriteLine(string.Join(", ", path));
+        }
+    }
+}
