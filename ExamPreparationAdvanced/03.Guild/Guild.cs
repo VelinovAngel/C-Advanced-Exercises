24cd6a4d9fcf9abe97670c6e64d9cc8b7ad30ab8
Angel <kakarum1992@gmail.com>
2020-10-21 22:50:45 +0200

    Exam Prep

---
+++ ExamPreparationAdvanced/03.Guild/Guild.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Guild
+{
+    public class Guild
+    {
+
+        private List<Player> players;
+
+        public Guild(string name, int capacity)
+        {
+            Name = name;
+            Capacity = capacity;
+            players = new List<Player>();
+        }
+
+        public int Count
+            => players.Count;
+
+        public string Name { get; set; }
+
+        public int Capacity { get; set; }
+
+
+        public void AddPlayer(Player player)
+        {
+            if (players.Count < Capacity)
+            {
+                players.Add(player);
+            }
+        }
+
+        public bool RemovePlayer(string name)
+        {
+            Player player = players.FirstOrDefault(x => x.Name == name);
+            return players.Remove(player);
+        }
+
+        public void PromotePlayer(string name)
+        {
+            Player player = players.FirstOrDefault(x => x.Name == name);
+            if (player.Rank != "Member")
+            {
+                player.Rank = "Member";
+            }
+        }
+
+        public void DemotePlayer(string name)
+        {
+            Player player = players.FirstOrDefault(x => x.Name == name);
+            if (player.Rank != "Trial")
+            {
+                player.Rank = "Trial";
+            }
+        }
+
+        public Player[] KickPlayersByClass(string name)
+        {
+
+            Player[] player = players.Where(x => x.Class.Contains(name))
+                                     .ToArray();
+
+            players = players.Where(x => !x.Class.Contains(name))
+                                     .ToList();
+            return player;
+        }
+
+        public string Report()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"Players in the guild: {Name}");
+            foreach (var player in players)
+            {
+                sb.AppendLine($"{player}");
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
--- ExamPreparationAdvanced/03.Guild/Player.cs
+++ ExamPreparationAdvanced/03.Guild/Player.cs
@@ -1,10 +1,57 @@
 ﻿using System;
-namespace _03.Guild
+using System.Text;
+
+namespace Guild
 {
     public class Player
     {
-        public Player()
+        private string rank = "Trial";
+        private string description = "n/a";
+
+        public Player(string name, string @class)
         {
+            Name = name;
+            Class = @class;
         }
+
+        public string Name { get; set; }
+
+        public string Class { get; set; }
+
+        public string Rank
+        {
+            get
+            {
+                return rank;
+            }
+            set
+            {
+                rank = value;
+            }
+        }
+
+        public string Description
+        {
+            get
+            {
+                return description;
+            }
+            set
+            {
+                description = value;
+            }
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine($"Player {Name}: {Class}")
+                .AppendLine($"Rank: {Rank}")
+                .AppendLine($"Description: {Description}");
+
+            return sb.ToString().TrimEnd();
+        }
+
     }
 }
