e6d5bc481a0868bbc1b0bf0663a31e611f7de314
Angel <kakarum1992@gmail.com>
2020-10-22 21:30:53 +0200

    Exam prep

--- ExamPreparationAdvanced/Christmas/Bag.cs
+++ ExamPreparationAdvanced/Christmas/Bag.cs
@@ -1,10 +1,64 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
 namespace Christmas
 {
     public class Bag
     {
-        public Bag()
+        private List<Present> presents;
+
+        public Bag(string color, int capacity)
         {
+            Color = color;
+            Capacity = capacity;
+            presents = new List<Present>();
+        }
+
+        public int Count
+            => presents.Count;
+
+        public string Color { get; set; }
+
+        public int Capacity { get; set; }
+
+
+        public void Add(Present present)
+        {
+            if (presents.Count < Capacity)
+            {
+                presents.Add(present);
+            }
+        }
+
+        public bool Remove(string name)
+        {
+            return presents.Remove(presents.FirstOrDefault(x => x.Name == name));
+        }
+
+        public Present GetHeaviestPresent()
+        {
+            return presents.FirstOrDefault(x => x.Weight == presents.Max(x => x.Weight));
+        }
+
+        public Present GetPresent(string name)
+        {
+            return presents.FirstOrDefault(x => x.Name == name);
+        }
+
+        public string Report()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine($"{Color} bag contains:");
+
+            foreach (var present in presents)
+            {
+                sb.AppendLine($"{present}");
+            }
+
+            return sb.ToString().TrimEnd();
         }
     }
 }
--- ExamPreparationAdvanced/Christmas/Present.cs
+++ ExamPreparationAdvanced/Christmas/Present.cs
@@ -3,8 +3,23 @@ namespace Christmas
 {
     public class Present
     {
-        public Present()
+        public Present(string name, double weight, string gender)
         {
+            Name = name;
+            Weight = weight;
+            Gender = gender;
         }
+
+        public string Name { get; set; }
+
+        public double Weight { get; set; }
+
+        public string Gender { get; set; }
+
+        public override string ToString()
+        {
+            return $"Present {Name} ({Weight}) for a {Gender}";
+        }
+
     }
 }
