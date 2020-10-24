54fe9eeb588c0d8ccd6192b79c0c0c7a942cd73d
Angel <kakarum1992@gmail.com>
2020-10-24 17:45:13 +0200

    Exam Prep

---
+++ ExamPreparationAdvanced/Rabbits/Cage.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Rabbits
+{
+    public class Cage
+    {
+        private List<Rabbit> rabbits;
+
+        public Cage(string name, int capacity)
+        {
+            Name = name;
+            Capacity = capacity;
+            rabbits = new List<Rabbit>();
+        }
+
+        public int Count
+            => rabbits.Count;
+
+        public string Name { get; set; }
+
+        public int Capacity { get; set; }
+
+        public void Add(Rabbit rabbit)
+        {
+            if (rabbits.Count < Capacity)
+            {
+                rabbits.Add(rabbit);
+            }
+        }
+
+        public bool RemoveRabbit(string name)
+        {
+            return rabbits.Remove(rabbits.FirstOrDefault(x => x.Name == name));
+        }
+
+        public void RemoveSpecies(string species)
+        {
+            rabbits.RemoveAll(x => x.Species == species);
+        }
+
+        public Rabbit SellRabbit(string name)
+        {
+            Rabbit rabbit = rabbits.FirstOrDefault(x => x.Name == name);
+            rabbit.Available = false;
+            return rabbit;
+        }
+
+        public Rabbit[] SellRabbitsBySpecies(string species)
+        {
+            Rabbit[] rabbs = rabbits.Where(x => x.Species == species).ToArray();
+            foreach (var rabbit in rabbs)
+            {
+                rabbit.Available = false;
+            }
+            return rabbs;
+        }
+
+        public string Report()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine("Rabbits available at {cageName}:");
+            foreach (var rabbit in rabbits.Select(x=>x.Available != false))
+            {
+                sb.AppendLine($"{rabbit}");
+            }
+
+            return sb.ToString().TrimEnd();
+
+        }
+    }
+}
---
+++ ExamPreparationAdvanced/Rabbits/Rabbit.cs
@@ -0,0 +1,36 @@
+﻿using System;
+namespace Rabbits
+{
+    public class Rabbit
+    {
+        private bool available = true;
+
+        public Rabbit(string name, string species)
+        {
+            Name = name;
+            Species = species;
+        }
+
+        public string Name { get; set; }
+
+        public string Species { get; set; }
+
+        public bool Available
+        {
+            get
+            {
+                return available;
+            }
+            set
+            {
+                available = value;
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"Rabbit ({Species}): {Name}";
+        }
+
+    }
+}
