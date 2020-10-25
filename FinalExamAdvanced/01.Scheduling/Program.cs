ece46b6b5c91d1e9dcda4009a1b25b4570548d51
Angel <kakarum1992@gmail.com>
2020-10-25 19:07:17 +0100

    Final Exam Advanced 25 Oct 2020

---
+++ FinalExamAdvanced/01.Scheduling/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01.Scheduling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] taskItems = Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            int[] threadsItems = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            int taskValue = int.Parse(Console.ReadLine());
+
+            Stack<int> stack = new Stack<int>(taskItems);
+            Queue<int> queue = new Queue<int>(threadsItems);
+
+
+            while (true)
+            {
+                int task = stack.Peek();
+                int threads = queue.Peek();
+                int sum = task + threads;
+
+                if (task == taskValue)
+                {
+                    Console.WriteLine($"Thread with value {threads} killed task {taskValue}");
+                    break;
+                }
+
+                if (threads >= task)
+                {
+                    stack.Pop();
+                    queue.Dequeue();
+                    continue;
+                }
+
+                if (threads < task)
+                {
+                    queue.Dequeue();
+                    continue;
+                }
+            }
+
+
+            if (queue.Count > 0)
+            {
+                Console.WriteLine($"{string.Join(" ", queue)}");
+            }
+        }
+    }
+}
---
+++ FinalExamAdvanced/ClassroomProject/Classroom.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ClassroomProject
+{
+    public class Classroom
+    {
+        private List<Student> students;
+
+        public Classroom(int capacity)
+        {
+            Capacity = capacity;
+            students = new List<Student>();
+        }
+
+        public int Capacity { get; set; }
+
+        public int Count
+            => students.Count;
+
+        public string RegisterStudent(Student student)
+        {
+            if (students.Count < Capacity)
+            {
+                students.Add(student);
+                return $"Added student {student.FirstName} {student.LastName}";
+            }
+            else
+            {
+                return "No seats in the classroom";
+            }
+        }
+
+        public string DismissStudent(string firstName, string lastName)
+        {
+            Student student = students.FirstOrDefault(x => x.FirstName == firstName && x.LastName == lastName);
+
+            if (student != null)
+            {
+                students.Remove(student);
+                return $"Dismissed student {firstName} {lastName}";
+            }
+            else
+            {
+                return "Student not found";
+            }
+        }
+
+        public string GetSubjectInfo(string subject)
+        {
+            //Student student = students.FirstOrDefault(x => x.Subject == subject);
+
+            Student[] student = students.Where(x => x.Subject.Contains(subject))
+                                     .ToArray();
+
+            StringBuilder sb = new StringBuilder();
+            if (student.Length > 0)
+            {
+                sb.AppendLine($"Subject: {subject}")
+                .AppendLine("Students:");
+
+                foreach (var stud in student)
+                {
+                    sb.AppendLine($"{stud.FirstName} {stud.LastName}");
+                }
+
+                return sb.ToString().TrimEnd();
+            }
+            else
+            {
+                return "No students enrolled for the subject";
+            }
+        }
+
+
+        public Student GetStudent(string firstName, string lastName)
+        {
+            Student student = students.FirstOrDefault(x => x.FirstName == firstName && x.LastName == lastName);
+
+            return student;
+        }
+
+
+    }
+}
